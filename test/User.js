cd07e5a3c605eeee1476a5eeffc751b441783282
chao <luanxuechao520@qq.com>
2017-05-18 14:49:06 +0800

    add unit test

---
+++ test/User.js
@@ -0,0 +1,48 @@
+/**
+ * Created by chao on 2017/5/18.
+ */
+var should = require('should');
+var sails = require('sails');
+var rc = require('rc');
+var superagent = require('superagent');
+
+describe('myapp.js', function () {
+  this.timeout(5000)
+  before(function (done) {
+    sails.lift({
+      log: {
+        level: 'error'   //指定错误级别，避免出现调试输出，这主要是用来调试controller
+      }
+    }, function (err, server) {
+      sails = server;
+      if (err)
+        return done(err);
+      done(err, sails);
+    });
+  });
+  after(function (done) {
+    var done_called = false;
+    sails.lower(function () {
+      if (!done_called) {
+        done_called = true;
+        setTimeout(function () {
+          sails.log.debug("inside app.lower, callback not called yet. calling.");
+          done();
+        }, 1000);
+      } else {
+        sails.log.debug("inside app.lower, callback already called.");
+      }
+    });
+  });
+  it('#login', function (done) {
+    var agent = superagent.agent;
+    agent(sails.hooks.http.app).get('http://localhost:1337/user/login?userName=luan&password=luan').end(function (err, res) {
+      // console.log(err, res)
+
+      should(res.body).be.have.property('id');
+      should(res.body).be.have.property('userName');
+      should(res.body).be.have.property('password');
+      return done();
+    });
+  })
+})
---
+++ test/test.js
@@ -0,0 +1,14 @@
+/**
+ * Created by chao on 2017/5/17.
+ */
+var should = require('should');
+
+
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      // (5).should.be.exactly(5).and.be.a.Number();
+      should(10).be.exactly(10)
+    });
+  });
+});
